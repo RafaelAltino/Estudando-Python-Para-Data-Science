365c5bec08b04746a97c8e670e2a6d7d11a6edb6
Rafael Felipe Reis Altino <rafaelfraltino@gmail.com>
2021-09-08 16:48:41 -0300

    Exercicios de matplotlib e testes com Pokemon

--- Pandas/teste2.py
+++ Pandas/teste2.py
@@ -1,13 +1,6 @@
 import numpy as np
 import pandas as pd
 
-estados = ['MG', 'MG', 'MG', 'SP', 'SP', 'SP', 'ES', 'ES', 'ES']
-cidades = ['Caratinga', 'Ipatinga', 'Timóteo', 'Sorocaba', 'Diadema', 'Piracicaba', 'Vila Velha', 'Vitória', 'Guarapari']
-dados = [[1848, 92603], [1964, 265409], [1964, 90568], [1654, 687357], [1959, 426757], [1767, 407252], [1535, 501325], [1551, 365855], [1860, 126701]]
+pkm = pd.read_excel('Tabela Pokemon.xlsx')
 
-multindex = list(zip(estados, cidades))
-multindex = pd.MultiIndex.from_tuples(multindex)
-
-df = pd.DataFrame(dados, multindex, ['Fundação', 'População'])
-
-print(df)
+pkm['Atc']  & pkm['Tipo 2'] == 'Flying'
